6246059718e0c98b369a8e384c9f1b1b3489fb7e
Keith.Mastranunzio <keith.mastranunzio@nike.com>
2015-10-29 12:06:27 -0700

    mods for dynamic stationid

--- express.js
+++ express.js
@@ -3,10 +3,17 @@ var app = express();
 var bouyData = require('./bdata.js');
 var cron = require('cron');
 
+var cronStation;
+
+function setCronStationId(stationId)
+{
+    cronStation = stationId;
+}
+
 var cronJob = cron.job("0 */1 * * * *", function(){
     // perform operation e.g. GET request http.get() etc.
-    console.log("acquiring BouyData...");
-    bouyData.initBouyData();
+    console.log("acquiring BouyData for station: " + cronStation);
+    bouyData.initBouyData(cronStation);
 }); 
 
 app.get('/bouyData', function (req, res) {
@@ -17,13 +24,16 @@ app.get('/bouyDataStatus', function (req, res) {
   res.send(bouyData.getBouyDataStatus());
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function () 
+{
+  var stationId = '46229';
 
-  bouyData.initBouyData('46229');
+  bouyData.initBouyData(stationId);
   
   var host = server.address().address;
   var port = server.address().port;
 
+  setCronStationId(stationId);
   cronJob.start();
 
   console.log('Example app listening at http://%s:%s', host, port);
